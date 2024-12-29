661e8aabdda1b4942e34e759246fcac1e8af60fd
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 19:40:08 +0100

    first commit

---
+++ csharp-hashset-es1/Program.cs
@@ -0,0 +1,10 @@
+﻿// dichiaro un HashSet di stringhe
+var animals = new HashSet<string>() { "Cane", "Gatto", "Elefante", "Leone" };
+// rimuovo un elemento specifico
+animals.Remove("Elefante");
+// stampo il HashSet aggiornato
+Console.WriteLine("HashSet aggiornato:");
+foreach (string animal in animals)
+{
+    Console.WriteLine(animal);
+}
